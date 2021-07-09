bad1bdd441f792bdf4b87a52cf0e306cea58a2a2
Jakub Widurek <jakubwidurek@gmail.com>
2021-07-09 16:21:10 +0200

    Updates

---
+++ precache-manifest.e986155a76891a286ec012fa9a1bd81f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0a5a84648b46088fade",
+    "url": "/ReactApp-Fetch-Example/static/css/main.eb072832.chunk.css"
+  },
+  {
+    "revision": "d0a5a84648b46088fade",
+    "url": "/ReactApp-Fetch-Example/static/js/main.d0a5a846.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/ReactApp-Fetch-Example/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "40449c74ae0f0b33fc4c",
+    "url": "/ReactApp-Fetch-Example/static/js/runtime~main.40449c74.js"
+  },
+  {
+    "revision": "4aa091157bbbc64391b8ea931b742a98",
+    "url": "/ReactApp-Fetch-Example/index.html"
+  }
+];
